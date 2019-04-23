9a52238ad45b56127f2feab3fd043c574e2a6f79
Daweihao <421679009@qq.com>
2019-04-23 17:47:45 -0400

    guessMoves before each step

--- src/aiTicTacToe.java
+++ src/aiTicTacToe.java
@@ -1,5 +1,16 @@
 import java.util.*;
 public class aiTicTacToe {
+	public final int NEGATIVE_INFINITY = Integer.MIN_VALUE;
+	public final int INFINITY = Integer.MAX_VALUE;
+    public class tttTree{
+    	private tttTree parent;
+    	public List<positionTicTacToe> view;
+    	public positionTicTacToe pos;
+    	public List<tttTree> children;
+    	public int value;
+	}
+
+
 
 	public int player; //1 for player 1 and 2 for player 2
 	private int getStateOfPositionFromBoard(positionTicTacToe position, List<positionTicTacToe> board)
@@ -12,7 +23,8 @@ public class aiTicTacToe {
 	{
 		//TODO: this is where you are going to implement your AI algorithm to win the game. The default is an AI randomly choose any available move.
 		positionTicTacToe myNextMove = new positionTicTacToe(0,0,0);
-		
+		List<positionTicTacToe> copied = deepCopyATicTacToeBoard(board);
+
 		do
 			{
 				Random rand = new Random();
@@ -24,6 +36,69 @@ public class aiTicTacToe {
 		return myNextMove;
 			
 		
+	}
+
+	private List<positionTicTacToe> deepCopyATicTacToeBoard(List<positionTicTacToe> board)
+	{
+		//deep copy of game boards
+		List<positionTicTacToe> copiedBoard = new ArrayList<positionTicTacToe>();
+		for(int i=0;i<board.size();i++)
+		{
+			copiedBoard.add(new positionTicTacToe(board.get(i).x,board.get(i).y,board.get(i).z,board.get(i).state));
+		}
+		return copiedBoard;
+	}
+
+	// produce a sequence of possible results from already known board
+	public List<List<positionTicTacToe>> guessMoves(List<positionTicTacToe> board){
+		List<List<positionTicTacToe>> moves = new ArrayList<>();
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				for (int k = 0; k < 4; k++) {
+					int location = i * 16 + j * 4 + k;
+					positionTicTacToe temp = board.get(location);
+					if (temp.state == -1 || temp.state == 0){
+						List<positionTicTacToe> temp_board = deepCopyATicTacToeBoard(board);
+						temp.state = player;
+						temp_board.set(location, temp);
+						moves.add(temp_board);
+					}
+				}
+			}
+		}
+		return moves;
+	}
+
+	public int miniMaxEach(tttTree node, int depth, boolean maximizer){
+		if (depth == 0 || node.children == null)
+			return node.value;
+		int value;
+		if (maximizer){
+			value = NEGATIVE_INFINITY;
+			for (tttTree each : node.children){
+				value = value > miniMaxEach(each, depth - 1, false)? value : miniMaxEach(each, depth - 1, false);
+			}
+			return value;
+		}else {
+			value = INFINITY;
+			for (tttTree each : node.children){
+				value = value > miniMaxEach(each, depth - 1, false)? value : miniMaxEach(each, depth - 1, true);
+			}
+			return value;
+		}
+	}
+
+	public positionTicTacToe randomAlgorithm(List<positionTicTacToe> board, int player){
+		positionTicTacToe myNextMove = new positionTicTacToe(0,0,0);
+		do
+		{
+			Random rand = new Random();
+			int x = rand.nextInt(4);
+			int y = rand.nextInt(4);
+			int z = rand.nextInt(4);
+			myNextMove = new positionTicTacToe(x,y,z);
+		}while(getStateOfPositionFromBoard(myNextMove,board)!=0);
+		return myNextMove;
 	}
 	private List<List<positionTicTacToe>> initializeWinningLines()
 	{
